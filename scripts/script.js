1efcf0d8347ba91ac15a7579ddbf05a4c76627d1
Tom <80857044+ike-tom@users.noreply.github.com>
2021-04-07 14:34:12 +0200

    script added

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+const hoursHand = document.querySelector(".hour-hand")
+const minutesHand = document.querySelector(".min-hand")
+const secondsHand = document.querySelector(".second-hand")
+
+function countingTime() {
+    const date = new Date
+    hoursHand.style.transformOrigin = 'center right'
+    hoursHand.style.transform = 'rotate(' + (date.getHours() + 3) * 30 + 'deg)'
+    minutesHand.style.transformOrigin = 'center right'
+    minutesHand.style.transform = 'rotate(' + (date.getMinutes() + 15) * 6 + 'deg)'
+    secondsHand.style.transformOrigin = 'center right'
+    secondsHand.style.transform = 'rotate(' + (date.getSeconds() + 15) * 6 + 'deg)'
+}
+
+window.setInterval(countingTime, 1000)
