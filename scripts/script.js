c63a32551bc1954d9286ac17ecc68e2235787528
Tom <80857044+ike-tom@users.noreply.github.com>
2021-04-07 14:35:33 +0200

    function naming adjustment

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@ const hoursHand = document.querySelector(".hour-hand")
 const minutesHand = document.querySelector(".min-hand")
 const secondsHand = document.querySelector(".second-hand")
 
-function countingTime() {
+function countTime() {
     const date = new Date
     hoursHand.style.transformOrigin = 'center right'
     hoursHand.style.transform = 'rotate(' + (date.getHours() + 3) * 30 + 'deg)'
@@ -12,4 +12,4 @@ function countingTime() {
     secondsHand.style.transform = 'rotate(' + (date.getSeconds() + 15) * 6 + 'deg)'
 }
 
-window.setInterval(countingTime, 1000)
+window.setInterval(countTime, 1000)
